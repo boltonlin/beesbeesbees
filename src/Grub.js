1395fcfdf88a2d01f671910170dce3f9a43c3ecc
boltonlin <boltonlinbo@gmail.com>
2022-12-19 10:10:21 -0800

    defined Grub class according to prompt

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,16 @@
 var Grub = function() {
+  this.species = 'Grub'
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
+
+Grub.prototype.eat = function () {
+  if (!!this.food) {
+    console.log(this.species + ' has eaten ' + this.food);
+    this.food = undefined;
+  } else {
+    console.log(this.species + ' has no food.');
+  }
+}
 
