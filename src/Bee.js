1c91c114e75797f661de037f60a22212eed0c293
boltonlin <boltonlinbo@gmail.com>
2022-12-19 10:29:54 -0800

    defined Bee class according to prompt

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);
+  this.species = 'bee';
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  console.log(this);
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
